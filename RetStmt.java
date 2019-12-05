a5bbedca495960b4f56a231010f15e8c8f9e6781
mcupka <michaelcupka@hotmail.com>
2019-12-04 19:58:22 -0500

    Ret and Subr gencode() hardcoded call/ret stuff

--- RetStmt.java
+++ RetStmt.java
@@ -10,9 +10,29 @@ class RetStmt extends Stmt {
         ArgObj args = parser.parse(tokens);
         NullArgObj nargs = (NullArgObj) args;
 
+        //ret will only occur once at the end of the program. Always does push 0, popa, ret
+        byte [] codebytes = new byte [7];
 
-        byte [] codebytes = new byte [1];
-        codebytes[0] = (byte) 0;
+
+        //pushi
+        codebytes[0] = (byte) 70;
+
+        //convert 0 into bytes
+        ByteBuffer b = ByteBuffer.allocate(4);
+        b.putInt(0);
+        byte[] intbytes = b.array();
+
+        codebytes[1] = intbytes[3];
+        codebytes[2] = intbytes[2];
+        codebytes[3] = intbytes[1];
+        codebytes[4] = intbytes[0];
+
+
+        //popa
+        codebytes[5] = (byte) 77;
+
+        //ret
+        codebytes[6] = (byte) 48;
 
         return codebytes;
     }
--- SubrStmt.java
+++ SubrStmt.java
@@ -11,7 +11,56 @@ public class SubrStmt extends Stmt {
         ArgObj args = parser.parse(tokens);
         SIArgObj siargs = (SIArgObj) args;
 
-        byte [] codebytes = null;
+
+        //Subr will always do: push 16, push 17, push 1, call, halt; Since main will be the only subr
+        byte [] codebytes = new byte[17];
+
+        //pushi
+        codebytes[0] = (byte) 70;
+
+        //convert 16 into bytes
+        ByteBuffer b = ByteBuffer.allocate(4);
+        b.putInt(16);
+        byte[] intbytes = b.array();
+
+        codebytes[1] = intbytes[3];
+        codebytes[2] = intbytes[2];
+        codebytes[3] = intbytes[1];
+        codebytes[4] = intbytes[0];
+
+        //pushi
+        codebytes[5] = (byte) 70;
+
+        //convert 17 into bytes
+        ByteBuffer b = ByteBuffer.allocate(4);
+        b.putInt(17);
+        byte[] intbytes = b.array();
+
+        codebytes[6] = intbytes[3];
+        codebytes[7] = intbytes[2];
+        codebytes[8] = intbytes[1];
+        codebytes[9] = intbytes[0];
+
+
+        //pushi
+        codebytes[10] = (byte) 70;
+
+        //convert 1 into bytes
+        ByteBuffer b = ByteBuffer.allocate(4);
+        b.putInt(1);
+        byte[] intbytes = b.array();
+
+        codebytes[11] = intbytes[3];
+        codebytes[12] = intbytes[2];
+        codebytes[13] = intbytes[1];
+        codebytes[14] = intbytes[0];
+
+
+        //call
+        codebytes[15] = (byte) 44;
+
+        //halt
+        codebytes[16] = (byte) 0;
 
         return codebytes;
     }
