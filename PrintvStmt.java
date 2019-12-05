48cded609f538d48bd68295fb3a6405d51ae5ce2
mcupka <michaelcupka@hotmail.com>
2019-12-04 21:10:53 -0500

    Printv done

--- PrintvStmt.java
+++ PrintvStmt.java
@@ -1,3 +1,5 @@
+import java.nio.ByteBuffer;
+
 class PrintvStmt extends Stmt {
 
     public PrintvStmt() {
@@ -8,7 +10,7 @@ class PrintvStmt extends Stmt {
     public byte [] genCode(String[] tokens) {
 
         ArgObj args = parser.parse(tokens);
-        SArgObj nargs = (SArgObj) args;
+        SArgObj sargs = (SArgObj) args;
 
         //printv does: pushi (int from symbol table), pushvi, printi
         byte [] codebytes = new byte [7];
@@ -19,13 +21,20 @@ class PrintvStmt extends Stmt {
 
         //convert (valuefromsymboltable) into bytes
         ByteBuffer b = ByteBuffer.allocate(4);
-        b.putInt(0);
+        b.putInt(StatementFactory.variables.get(sargs.str));
         byte[] intbytes = b.array();
 
         codebytes[1] = intbytes[3];
         codebytes[2] = intbytes[2];
         codebytes[3] = intbytes[1];
         codebytes[4] = intbytes[0];
+
+
+        //pushvi
+        codebytes[5] = (byte) 74; 
+
+        //printi
+        codebytes[6] = (byte) 146; 
 
         return codebytes;
     }
